098a4dbe6dd68f0996421f2bfd7c1fd542949bea
kingsman142 <kingsman142@yahoo.com>
2016-08-30 23:24:30 -0400

    added image toggle on computer screen; added ludum dare tile; added slide toggles for sections

--- index.js
+++ index.js
@@ -4,12 +4,31 @@ $(document).ready(function(){
 			this.style.visibility = "visible";
 		});
 	});
-});
 
-$(document).ready(function(){
 	$(".hoverable").mouseleave(function(){
 		$(".resume-buttons").each(function(){
 			this.style.visibility = "hidden";
 		});
+	});
+
+	$("#education-wrapper > .section-header").click(function(){
+		$("#school-div-main").slideToggle();
+	});
+
+	$("#experience-wrapper > .section-header").click(function(){
+		$("#experience-div-main").slideToggle();
+	});
+
+	$("#projects-wrapper > .section-header").click(function(){
+		$("#projects-div-main").slideToggle();
+	});
+
+	$(".action-button.animate.shadow.green").click(function(){
+		var picVis = $("#my-picture")[0].style.display;
+		if(picVis == "none" || picVis == ""){
+			$("#my-picture")[0].style.display = "inline";
+		} else if(picVis == "inline"){
+			$("#my-picture")[0].style.display = "none";
+		}
 	});
 });
